222169a2017f6ee2cf56f8eef9d44a7191bb0221
Liam Coogan <lcoogan@tcd.ie>
2025-02-15 13:15:38 +0000

    Trigger shortcuts on selection, then close window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,11 +4,16 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
+using Key_Wizard.shortcuts;
+using Key_Wizard.startup;
 using Microsoft.UI;
+using Microsoft.UI.Input;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
 using Windows.Graphics;
+using Windows.System;
 using WinRT.Interop;
 
 namespace Key_Wizard
@@ -17,6 +22,8 @@ namespace Key_Wizard
     {
         public string Prefix { get; set; }  // Bold part
         public string Suffix { get; set; }  // Normal part
+
+        public string Action { get; set; }  // Function to be triggered on click
     }
 
     public class Section
@@ -28,21 +35,16 @@ namespace Key_Wizard
     {
         private const Double MAX_HEIGHT = 0.3;
         private const Double MAX_WIDTH = 0.4;
-        private Dictionary<string, Action> actions;
         private Dictionary<string, CreateSections> shortcutDictionary;
 
         [DllImport("kernel32.dll")]
         static extern bool AllocConsole();
 
-        [DllImport("user32.dll")]
-        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, IntPtr dwExtraInfo);
-
-        private const int VK_TAB = 0x09;
-        private const int VK_MENU = 0x12; // Alt key
-        private const int KEYEVENTF_KEYUP = 0x0002;
-
         public MainWindow()
         {
+            // Uncomment the below line to spawn a console window
+            // AllocConsole();
+
             this.InitializeComponent();
 
             // Get the AppWindow for the current window
@@ -68,15 +70,6 @@ namespace Key_Wizard
 
             this.AppWindow.MoveAndResize(GetWindowSizeAndPos(0.3, 0.06));
 
-            actions = new Dictionary<string, Action>(StringComparer.OrdinalIgnoreCase)
-            {
-                {"Open the Run dialog box", runDialog},
-                {"Switch between open windows", altTab },
-                {"Capture a full screen screenshot", screenshot },
-                {"Open the Settings App", settings }
-            };
-
-            AllocConsole();
             shortcutDictionary = new Dictionary<string, CreateSections>();
             shortcutDictionary = CreateDictionary.InitList();
         }
@@ -125,32 +118,6 @@ namespace Key_Wizard
             ));
         }
 
-        public void runDialog()
-        {
-            Process.Start("explorer.exe", "shell:::{2559a1f3-21d7-11d4-bdaf-00c04f60b9f0}");
-        }
-
-        public void altTab()
-        {
-            keybd_event(VK_MENU, 0, 0, IntPtr.Zero);  // Press Alt
-            keybd_event(VK_TAB, 0, 0, IntPtr.Zero);   // Press Tab
-            keybd_event(VK_TAB, 0, KEYEVENTF_KEYUP, IntPtr.Zero);  // Release Tab
-            keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, IntPtr.Zero); // Release Alt
-        }
-
-        public void screenshot()
-        {
-            keybd_event(0x5B, 0, 0, IntPtr.Zero);     // Windows key down
-            keybd_event(0x2C, 0, 0, IntPtr.Zero);     // Print Screen
-            keybd_event(0x2C, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
-            keybd_event(0x5B, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
-        }
-
-        public void settings()
-        {
-            Process.Start("explorer.exe", "ms-settings:");
-        }
-
         private void searchTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             shortcutsList.Visibility = Visibility.Visible;
@@ -164,7 +131,7 @@ namespace Key_Wizard
 
                 foreach (var keyAction in section.Value.Data)
                 {
-                    ListItem newItem = new ListItem { Prefix = $"{keyAction.Key}: ", Suffix = $"{keyAction.Value}" };
+                    ListItem newItem = new ListItem { Prefix = $"{keyAction.Key}: ", Suffix = $"{keyAction.Value.action}", Action = $"{keyAction.Value.function}" };
                     items.Add(newItem);
                 }
 
@@ -176,5 +143,37 @@ namespace Key_Wizard
             // Resize the window
             AdjustWindowToContent();
         }
+
+        private void ListView_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if (e.Key == VirtualKey.Enter || e.Key == VirtualKey.Space)
+            {
+                var listView = (ListView)sender;
+                var item = (ListItem)listView.SelectedItem;
+                TriggerAction(item);
+            }
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var item = (ListItem)e.ClickedItem;
+            TriggerAction(item);
+        }
+
+        private void TriggerAction(ListItem item)
+        {
+            var methodInfo = typeof(Shortcuts).GetMethod(item.Action, System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+
+            if (methodInfo != null)
+            {
+                Action action = (Action)Delegate.CreateDelegate(typeof(Action), methodInfo);
+                action.Invoke();
+                this.Close();
+            }
+            // Right now there is no error handling for if the action doesn't exist as we do not want any
+            // errors during the demo. We will add some error handling once we have all the actions implemented.
+        }
+
+        
     }
 }
---
+++ shortcuts/Shortcuts.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Key_Wizard.shortcuts
+{
+    internal class Shortcuts
+    {
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, IntPtr dwExtraInfo);
+
+        private const int VK_TAB = 0x09;
+        private const int VK_MENU = 0x12; // Alt key
+        private const int KEYEVENTF_KEYUP = 0x0002;
+
+        // Opens Run dialog
+        public static void windowsKeyR()
+        {
+            Process.Start("explorer.exe", "shell:::{2559a1f3-21d7-11d4-bdaf-00c04f60b9f0}");
+        }
+
+        // Switches to previous window
+        public static void altTab()
+        {
+            keybd_event(VK_MENU, 0, 0, IntPtr.Zero);  // Press Alt
+            keybd_event(VK_TAB, 0, 0, IntPtr.Zero);   // Press Tab
+            keybd_event(VK_TAB, 0, KEYEVENTF_KEYUP, IntPtr.Zero);  // Release Tab
+            keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, IntPtr.Zero); // Release Alt
+        }
+
+        // Opens screenshot tool
+        public static void PrtSc()
+        {
+            keybd_event(0x5B, 0, 0, IntPtr.Zero);     // Windows key down
+            keybd_event(0x2C, 0, 0, IntPtr.Zero);     // Print Screen
+            keybd_event(0x2C, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
+            keybd_event(0x5B, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
+        }
+
+        // Opens the Settings menu
+        public static void windowsKeyI()
+        {
+            Process.Start("explorer.exe", "ms-settings:");
+        }
+    }
+}
